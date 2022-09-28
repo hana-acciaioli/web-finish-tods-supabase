71b120cbf399355b80cb604143dd008314cfcd34
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-28 14:58:54 -0700

    Part C: Complete Todo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 // Part A: import create todo
-import { createTodo, getTodos } from './fetch-utils.js';
+import { createTodo, getTodos, completeTodo } from './fetch-utils.js';
 // Part B: import get todos
 // Part C: import complete todos
 // Part D: import delete all function
@@ -91,12 +91,24 @@ function displayTodos() {
         todoList.append(todoEl);
 
         // > Part C: Add a click event listener for the todoEl
-        //      - call the async supabase function to delete all todos
-        //        and get the response
-        //      - if there's an error, set error state and call displayError
-        //      - otherwise:
-        //          - find the index of todo in todos
-        //          - update that index of todos with the response data
-        //          - redisplay the todos
+        todoEl.addEventListener('click', async () => {
+            //      - call the async supabase function to delete all todos
+            //        and get the response
+            const response = await completeTodo(todo.id);
+            error = response.error;
+            const updatedTodo = response.data;
+            //      - if there's an error, set error state and call displayError
+            if (error) {
+                displayError();
+                // - otherwise:
+                //          - find the index of todo in todos
+            } else {
+                const index = todos.indexOf(todo);
+                //          - update that index of todos with the response data
+                todos[index] = updatedTodo;
+                //          - redisplay the todos
+                displayTodos();
+            }
+        });
     }
 }
