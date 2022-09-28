d1e7790a2ee863b6851a6f1d0259e543f44ba725
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-28 15:10:51 -0700

    Part D: Delete All Users Todo

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 // Part A: import create todo
-import { createTodo, getTodos, completeTodo } from './fetch-utils.js';
-// Part B: import get todos
-// Part C: import complete todos
+import {
+    createTodo,
+    getTodos,
+    completeTodo,
+    deleteAllTodos,
+} from './fetch-utils.js';
+// Part B: import get todos ^
+// Part C: import complete todos ^
 // Part D: import delete all function
 import { renderTodo } from './render-utils.js';
 
@@ -61,14 +66,14 @@ addTodoForm.addEventListener('submit', async (e) => {
 
 removeButton.addEventListener('click', async () => {
     // > Part D: Call the async supabase function to delete all todos
-    const response = null; // change me
+    const response = await deleteAllTodos();
     error = response.error;
 
     if (error) {
         displayError();
     } else {
         // > Part D: reset todos state to an empty array:
-
+        todos = [];
         displayTodos();
     }
 });
--- fetch-utils.js
+++ fetch-utils.js
@@ -53,19 +53,23 @@ export async function deleteAllTodos() {
     const user = getUser();
 
     // > Part D: delete all todos for this user in supabase:
+    return await client
+        .from('todos')
+        .delete()
 
-    // Supabase doesn't allow deleting without a where clause,
-    // which is a good thing it most cases because we generally
-    // don't want to delete every single row from the database.
-    //
-    // Let's use this as an opportunity to talk about what
-    // Row Level Security (RLS) does for us automatically,
-    // which is appending a condition on the server that does
-    // what this ".eq" does.
-    //
-    // But the supabase client in the browser doesn't know about this,
-    // so it helps us by making sure we aren't deleting the whole table!
+        // Supabase doesn't allow deleting without a where clause,
+        // which is a good thing it most cases because we generally
+        // don't want to delete every single row from the database.
+        //
+        // Let's use this as an opportunity to talk about what
+        // Row Level Security (RLS) does for us automatically,
+        // which is appending a condition on the server that does
+        // what this ".eq" does.
+        //
+        // But the supabase client in the browser doesn't know about this,
+        // so it helps us by making sure we aren't deleting the whole table!
 
-    // Use this to add the user id as a "where" criteria filter:
-    // .eq('user_id', user.id);
+        // Use this to add the user id as a "where" criteria filter:
+        // .eq('user_id', user.id);
+        .eq('user_id', user.id);
 }
